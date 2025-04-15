0d42becc8031c9b22877351385ffd4499e3ad434
Prof. Cristiano de Paula <cristiano.paula@docente.senai.br>
2025-04-15 10:16:30 -0300

    Adicionado o serviço do CORS e Autenticação

--- APIEscola/Program.cs
+++ APIEscola/Program.cs
@@ -1,5 +1,6 @@
 
 using APIEscola.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,38 @@ var builder = WebApplication.CreateBuilder(args);
 // Adicionar Servico de banco de dados
 builder.Services.AddDbContext<APIEscolaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Adicionar o serviço de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+
+// Adicionar o serviço de autenticação
+// Serviço de EndPoints do Identity Framework
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false; // Exige confirmação de email
+    options.SignIn.RequireConfirmedAccount = false; // Exige confirmação de conta
+    options.User.RequireUniqueEmail = true; // Exige email único
+    options.Password.RequireNonAlphanumeric = false; // Exige caracteres não alfanuméricos
+    options.Password.RequireLowercase = false; // Exige letras minúsculas
+    options.Password.RequireUppercase = false; // Exige letras maiúsculas
+    options.Password.RequireDigit = false; // Exige dígitos numéricos
+    options.Password.RequiredLength = 4; // Exige comprimento mínimo da senha
+})
+.AddRoles<IdentityRole>() // Adicionando o serviço de roles
+.AddEntityFrameworkStores<APIEscolaContext>() // Adicionando o serviço de EntityFramework
+.AddDefaultTokenProviders(); // Adiocionando o provedor de tokens padrão
+
+
+
+
+
 
 
 builder.Services.AddControllers();
